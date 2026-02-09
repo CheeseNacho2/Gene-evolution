37e33696c82081c632321e5c8cd18ffa47b30c8c
CheeseNacho2 <owliefromthehood@gmail.com>
2026-02-09 01:41:43 +0100

    Uploading an ongoing project to GitHub

---
+++ GeneEVO/genesimulator/src/main/java/com/example/TreeBuilder.java
@@ -0,0 +1,15 @@
+/*public class TreeBuilder {
+
+    public static void growTree(int currentGeneration, int maxGenerations, Gene rootGene) {
+
+        if (currentGeneration > maxGenerations) {
+            return;
+        }
+
+        Gene currentGene = rootGene;
+
+        node.left = growTree(currentGeneration + 1, maxGenerations,currentGene);
+        node.right = growTree(currentGeneration + 1, maxGenerations, currentGene);
+
+    }
+}*/
